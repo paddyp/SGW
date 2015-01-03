dfd69915328bee4774854d232c0774021b4c24eb
paddyp <patrikpezelj@Patriks-MacBook-Pro-2.local>
2015-01-03 22:17:32 +0100

    INIT
    index.php wurde erzeugt und mit dem Grundgerüst der HTML Programmierung
    ausgestattet. Zu dem wurden die links zu 4 CSS Dateien gesetzt. Die css
    Dateien style,desktop,mobil,tablet wurden erstellt. Diese haben noch
    keinen Inhalt. Der Ordner img ist für Bilder erstellt worden. In dem
    Ordner befindet sich fürs erste das SG Wambach Logo.
    
    js Ordner angelegt. Version 2.1 von JQuery eingefügt.
    
    Dieser Stand wird hochgeladen

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>SG 1956 Wambach</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+<!-- Hauptgroessen -->
+
+<link href="css/desktop.css" 		rel="stylesheet" type="text/css" media="screen and (min-width: 1024px)"								/>
+<link href="css/tablet.css" 		rel="stylesheet" type="text/css" media="screen and (min-width: 600px) and (max-width: 1024px)" 	/>
+<link href="css/mobil.css" 			rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" 	/>
+
+<script src="js/jquery-2_1_0.js">
+</script>
+</head>
+
+<body>
+
+<h1><img src="img/wappen.jpg" width="50px"/> SG Wambach</h1>
+<p>
+Unter diesem link ist der aktuelle Stand der SG Wambach Seite einzusehen.
+</p>
+
+</body>
+</html>
