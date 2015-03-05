7948e55dc84ea6cdd66cfa49c2b48da8cc266aa0
paddyp <patrikpezelj@Patriks-MacBook-Pro-2.local>
2015-03-05 18:43:52 +0100

    Termine
    Die Termine sind nicht ganz so haesslich geworden

--- inc/startseite.php
+++ inc/startseite.php
@@ -1,3 +1,6 @@
 <div id="startseite">
 	<img src="img/content.png" />
+    <img src="img/Termine.png" style="-webkit-box-shadow: 0px 0px 5px 5px rgba(0,0,0,0.20);
+-moz-box-shadow: 0px 0px 5px 5px rgba(0,0,0,0.2);
+box-shadow: 0px 0px 5px 5px rgba(0,0,0,0.2);"/>
 </div>
